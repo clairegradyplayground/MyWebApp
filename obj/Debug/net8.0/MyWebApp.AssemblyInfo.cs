e812ebc43c0334a1ee49866328207f526b809900
clairegradyplayground <clairegrady@icloud.com>
2024-03-28 11:09:13 +1100

    minor change

--- obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb801ae28e2341cd6bdecc2c8920d7870d5e1a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a83afdec5c14aa5e4eb24b082890cd8e61b228")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
